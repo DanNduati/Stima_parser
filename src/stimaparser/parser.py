96b06a6ea6f7946b6545df31db48147eaf0723c4
DanielNduati <dannduati2@gmail.com>
2022-07-14 17:13:13 +0300

    Implement a region parser
    - Use the finditer() re module function that yields the match objects in the text from the regex
    - Todo: Document the regex

--- src/stimaparser/parser.py
+++ src/stimaparser/parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from pathlib import Path
 import re
+from pathlib import Path
+
 from pdfminer.high_level import extract_text
 
 PDF_FILE_PATH = "/home/daniel/Desktop/learn/stima_parser/pdfs"
@@ -14,6 +15,20 @@ def extract_pdftext(pdf_path: Path) -> str:
     return text
 
 
+def parse_regions(text: str):
+    regex = r"(?P<REGION>[A-Z]{2,}\s?[A-Z]{2,})+\s+REGION"
+    matches = re.finditer(regex, text, re.MULTILINE)
+
+    for match_num, match in enumerate(matches, start=1):
+        print(
+            f"Region Match {match_num} at {match.start()}-{match.end()}: {match.group()}"
+        )
+        # Region -> group 1
+        print(
+            f"Actual Region found at {match.start(1)}-{match.end(1)}: {match.group('REGION')}"
+        )
+
+
 def main():
     pdf_files = Path(PDF_FILE_PATH).glob("**/*.pdf")
     for pdf_file in pdf_files:
@@ -22,6 +37,7 @@ def main():
         print(f"{'*'*len(str(pdf_file))}\n")
         text = extract_text(str(pdf_file))
         # print(repr(text))
+        parse_regions(text)
 
 
 if __name__ == "__main__":
